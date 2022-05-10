5bd8af753bd87d0a3308e426ebae9106ce09f52b
SissiHidalgo <shidalgoa1@gmail.com>
2022-05-09 22:00:59 -0500

    dev2commit

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 public class HelloJava {
-	//Comentario de prueba v1
+	//Comentario de prueba v1 para validar el conflicto
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
